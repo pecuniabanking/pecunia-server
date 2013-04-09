9ee06fe50425252fb027c3b5cb5964eff68d56d4
Mike Lischke <mike.lischke@gmail.com>
2013-04-09 09:06:12 +0200

    - Changed absolute references to relative ones to be able two build the server. - Some cleanup on the way.

--- src/XmlGen.java
+++ src/XmlGen.java
@@ -115,7 +115,7 @@ public class XmlGen {
     	tag("subNumber", account.subnumber);
         intTag("type", account.category);
         xmlBuf.append("<supportedJobs type=\"list\">");
-        ArrayList<String> gvs = (ArrayList<String>)account.allowedGVs;
+		ArrayList<String> gvs = (ArrayList<String>)account.allowedGVs;
         supportedJobsToXml(handler, gvs);
         xmlBuf.append("</supportedJobs>");
     	xmlBuf.append("</object>");
